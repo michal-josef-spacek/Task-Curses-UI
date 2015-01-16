7d53bf9dab8c31dec8050702ee295e778b5be162
Michal Špaček <tupinek@gmail.com>
2015-01-16 18:07:26 +0100

    First version.

---
+++ UI.pm
@@ -0,0 +1,57 @@
+package Task::Curses::UI;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Curses::UI - Install the Curses::UI modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::Curses::UI
+
+=head1 SEE ALSO
+
+L<Curses::UI>,
+L<Curses::UI::DelimitedTextViewer>,
+L<Curses::UI::Grid::Cell>,
+L<Curses::UI::Mousehandler::GPM>,
+L<Curses::UI::Number>,
+L<Curses::UI::POE>,
+L<Curses::UI::Time>,
+L<Curses::UI::Volume>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Task-Curses-UI>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
