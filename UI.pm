1b65b49eb419ce3632066f7faf664a7c2636aaa1
Michal Josef Špaček <tupinek@gmail.com>
2020-04-05 15:08:04 +0200

    Update author name

--- UI.pm
+++ UI.pm
@@ -189,13 +189,13 @@ L<https://github.com/michal-josef-spacek/Task-Curses-UI>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2015 Michal Špaček
+ © 2015 Michal Josef Špaček
  Artistic License
  BSD 2-Clause License
 
