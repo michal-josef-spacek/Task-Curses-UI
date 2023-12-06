1c24441fee1a21341fd6cecd28d56a16fc6bd684
Michal Josef Špaček <tupinek@gmail.com>
2023-12-06 12:27:48 +0100

    Prepare for 0.07 version

--- UI.pm
+++ UI.pm
@@ -3,7 +3,7 @@ package Task::Curses::UI;
 use strict;
 use warnings;
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 1;
 
@@ -211,6 +211,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
